896f1b766fdda2af721eecfaf9679272769a1c61
jsunga <jawyn.sunga@gmail.com>
2019-07-09 17:50:43 -0700

    add skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -31,6 +31,50 @@ const Intro = styled.div`
 `
 
 const Wrapper = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-size: 15px;
+  color: white;
+  padding-left: 10px;
+  padding-right: 10px;
+`
+
+const Row = styled.div`
+  display: flex;
+  padding: 10px;
+`
+
+const Col = styled.div`
+  background-color: #be2835;
+  border: 1px solid #be2835;
+  width: 33.33%;
+  border: 1px solid;
+  text-align: center;
+  margin: 2px;
+  border-radius: 5px;
+  padding: 10px;
+  transition: .8s;
+
+  &:hover {
+    background-color: white;
+    color: #be2835;
+  }
+`
+
+const Content = styled.div`
+  background-color: #be2835;
+  border: 1px solid #be2835;
+  width: 25%;
+  border: 1px solid;
+  text-align: center;
+  margin: 2px;
+  border-radius: 5px;
+  padding: 10px;
+  transition: .8s;
+
+  &:hover {
+    background-color: white;
+    color: #be2835;
+  }
 `
 
 export default class Skills extends Component {
@@ -38,13 +82,39 @@ export default class Skills extends Component {
     return (
       <Body id='skills'>
         <Container>
-          <Header className="wow bounceInDown">Technological Skills</Header>
-          <Intro className="wow fadeInLeft">
+          <Header>Technological Skills</Header>
+          <Intro>
             I always enjoy learning new things. 
             Especially the skills and technologies that make your company succeed.
           </Intro>
           <Wrapper>
-
+            <Row>
+              <Col>HTML</Col>
+              <Col>CSS</Col>
+              <Col>Sass</Col>
+            </Row>
+            <Row>
+              <Content>JavaScript</Content>
+              <Content>Python</Content>
+              <Content>Ruby</Content>
+              <Content>Java</Content>
+            </Row>
+            <Row>
+              <Col>React.js</Col>
+              <Col>Redux.js</Col>
+              <Col>Node.js</Col>
+            </Row>
+            <Row>
+              <Content>Django</Content>
+              <Content>Rails</Content>
+              <Content>SQL</Content>
+              <Content>NoSQL</Content>
+            </Row>
+            <Row>
+              <Col>Git</Col>
+              <Col>Linux</Col>
+              <Col>Microservices</Col>
+            </Row>
           </Wrapper>
         </Container>
       </Body>
