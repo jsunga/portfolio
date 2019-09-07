bbf5a22f989d201e73e4e1867b27e8a2d3847946
jsunga <jawyn.sunga@gmail.com>
2019-09-07 15:03:29 -0700

    Updates

---
+++ precache-manifest.2c40b0e559a256cb9c149bb6b1c30820.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f840c165c7bb8c3e749024634c9f87a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b6e1764e03a3c3d7e62e",
+    "url": "/portfolio/static/css/main.9ed3115e.chunk.css"
+  },
+  {
+    "revision": "7362a2536caf74d52839",
+    "url": "/portfolio/static/js/2.775dc841.chunk.js"
+  },
+  {
+    "revision": "b6e1764e03a3c3d7e62e",
+    "url": "/portfolio/static/js/main.a8d55531.chunk.js"
+  },
+  {
+    "revision": "ada1c731a65e33dd722e",
+    "url": "/portfolio/static/js/runtime~main.ed209e06.js"
+  },
+  {
+    "revision": "66728bfe969cb32a4860a45cff5daadd",
+    "url": "/portfolio/static/media/apples.66728bfe.png"
+  },
+  {
+    "revision": "de4a2a917352349bb164b588fa168dc6",
+    "url": "/portfolio/static/media/diaryhub.de4a2a91.png"
+  },
+  {
+    "revision": "73a63b97589411637b05d673d90b9790",
+    "url": "/portfolio/static/media/eye.73a63b97.png"
+  },
+  {
+    "revision": "160d18217d93ddb37c112318b960d688",
+    "url": "/portfolio/static/media/foodie.160d1821.png"
+  },
+  {
+    "revision": "40386b1b231ff745bd771a78df5f3e8a",
+    "url": "/portfolio/static/media/skyla.40386b1b.jpg"
+  },
+  {
+    "revision": "39a338beb734300cee052cfd5628beab",
+    "url": "/portfolio/static/media/twitter.39a338be.png"
+  },
+  {
+    "revision": "7b64ea30af801c92d6c38e99d3496f32",
+    "url": "/portfolio/static/media/zellow.7b64ea30.png"
+  }
+]);
